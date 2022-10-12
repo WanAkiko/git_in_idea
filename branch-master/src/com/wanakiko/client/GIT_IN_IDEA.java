8c7bcc4513e82262fee2ffa16fbcc67806927c86
wanakiko <3052410459@qq.com>
2022-10-12 09:29:16 +0800

    信息：第一次提交，从暂存区到本地库。

---
+++ branch-master/src/com/wanakiko/client/GIT_IN_IDEA.java
@@ -0,0 +1,15 @@
+package com.wanakiko.client;
+
+import java.util.UUID;
+
+/*
+ * @author: WanAkiko
+ * @date: 2022-10-12
+ * @description: 演示Git的高频命令
+ * @version:1.0
+ */
+public class GIT_IN_IDEA {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString().toUpperCase());
+    }
+}
