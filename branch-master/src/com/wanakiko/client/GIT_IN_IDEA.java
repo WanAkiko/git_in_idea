5dcb36c0d00f0bf8367d840ff6e3971a958a049f
wanakiko <3052410459@qq.com>
2022-10-12 22:50:44 +0800

    信息：develop分支的第一次提交，从暂存区到本地库。

--- branch-master/src/com/wanakiko/client/GIT_IN_IDEA.java
+++ branch-master/src/com/wanakiko/client/GIT_IN_IDEA.java
@@ -14,5 +14,6 @@ public class GIT_IN_IDEA {
         System.out.println(UUID.randomUUID().toString().toUpperCase());
         System.out.println(ThreadLocalRandom.current().nextInt(99) + 1);
         System.out.println("红色表示未追踪，绿色表示已追踪，黑色表示已提交，蓝色表示已提交且出现新的修改");
+        System.out.println("DEVELOP：鼠标右键选择Git，然后点击New Branch填写分支名称即可创建新的分支。");
     }
 }
