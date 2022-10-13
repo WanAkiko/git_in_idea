c692cf815d620f51ece821edd11e37d7439bbda4
wanakiko <71197860+WanAkiko@users.noreply.github.com>
2022-10-13 15:51:43 +0800

    信息：2022-10-13，在PC端网页修改当前类的内容，用于演示代码拉取与合并。

--- branch-master/src/com/wanakiko/client/GIT_IN_IDEA.java
+++ branch-master/src/com/wanakiko/client/GIT_IN_IDEA.java
@@ -18,5 +18,6 @@ public class GIT_IN_IDEA {
         System.out.println("MASTER：master分支与develop分支的同一个位置出现不同的修改。");
         System.out.println("DEVELOP：此处编辑的内容用于演示出现冲突时的分支合并。");
         System.out.println("IntelliJ IDEA Push code to Github.");
+        System.out.println("信息：2022-10-13，在PC端网页修改当前类的内容，用于演示代码拉取与合并。");
     }
 }
